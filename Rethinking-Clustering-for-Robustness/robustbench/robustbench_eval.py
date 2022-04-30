2f1acffe7941dee29af1b26e0a04a3a2661648d7
Ankit <bhardwaj.ankit275@gmail.com>
2022-04-30 17:37:35 -0400

    [MAJOR] corret evaluation code for clustering model

--- Rethinking-Clustering-for-Robustness/robustbench/robustbench_eval.py
+++ Rethinking-Clustering-for-Robustness/robustbench/robustbench_eval.py
@@ -19,6 +19,7 @@ from models.resnet import ResNet18
 # from utils.attacks import final_attack_eval
 # from utils.magnet_training import magnet_epoch_wrapper
 # from utils.setups import magnet_assertions, get_batch_builders, get_magnet_data
+from utils.utils import get_softmax_probs
 # from utils.logging import (report_epoch_and_save, print_to_log, update_log,
     # print_training_params, check_best_model, copy_best_checkpoint)
 from utils.utils import eval_model, compute_real_losses, copy_pretrained_model
@@ -31,14 +32,19 @@ from autoattack import AutoAttack
 cudnn.deterministic = True
 cudnn.benchmark = False
 
+
 class model_for_robustbench(nn.Module):
-    def __init__(self, model):
+    def __init__(self, model, magnet_data):
         super(model_for_robustbench, self).__init__()
         self.model = model
+        self.magnet_data = magnet_data
 
     def forward(self, x):
         logits, embedding = self.model(x)
-        return(logits)
+        if(self.magnet_data is None):
+            return(logits)
+        else:
+            return(get_softmax_probs(embedding, self.magnet_data, return_scores=True))
 
 
 def main():
@@ -50,6 +56,9 @@ def main():
                         help='path to model')
 
     args = parser.parse_args()
+    np.random.seed(args.seed)
+    # torch.manual_seed(args.seed)
+    # torch.cuda.manual_seed(args.seed)
     # Decide device to use
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     if device == 'cuda':
@@ -57,14 +66,32 @@ def main():
     model = ResNet18(num_classes=10).to(device)
     if args.model_path is not None:
         model = copy_pretrained_model(model, args.model_path)
-    eval_model = model_for_robustbench(model)
+    
+    ckpt = torch.load(args.model_path)
+    try:
+        magnet_data = {
+            'cluster_classes'   : ckpt['cluster_classes'],
+            'cluster_centers'   : ckpt['cluster_centers'],
+            'variance'          : ckpt['variance'],
+            'L'                 : ckpt['L'],
+            # 'L'                 : 10,
+            'K'                 : ckpt['K'],
+            'normalize_probs'   : ckpt['normalize_probs'],
+            # 'normalize_probs'   : True,
+        }
+        print('Succesfully loaded magnet_data from checkpoint')
+    except:
+        magnet_data = None
+        print('Unable to load magnet_data from checkpoint. '
+            'Regular training is inferred')
+    eval_model = model_for_robustbench(model, magnet_data)
     x_test, y_test = load_cifar10(n_examples=10000)
     # threat_model='Linf'
     # adversary = AutoAttack(eval_model, norm='Linf', eps=8/255, version='custom', attacks_to_run=['apgd-ce', 'apgd-dlr'])
     adversary = AutoAttack(eval_model, norm='Linf', eps=8/255)
     adversary.apgd.n_restarts = 1
     x_adv = adversary.run_standard_evaluation(x_test, y_test)
-    print(x_adv)
+    # print(x_adv)
 
 if __name__ == '__main__':
     main()
